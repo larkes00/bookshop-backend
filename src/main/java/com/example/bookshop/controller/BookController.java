805cd261d77e90f0f18cb9a5355925b8c528df68
larkes00 <zakirov2802@gmail.com>
2021-10-15 18:13:42 +0300

    Creating Rest Controllers

--- src/main/java/com/example/bookshop/controller/BookController.java
+++ src/main/java/com/example/bookshop/controller/BookController.java
@@ -1,4 +1,9 @@
 package com.example.bookshop.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
 public class BookController {
 }
