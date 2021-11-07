cb6bcd8d33a6200ae1d31cd3372959d480b91ecc
larkes00 <zakirov2802@gmail.com>
2021-11-07 16:47:46 +0200

    Created exception

---
+++ src/main/java/com/example/bookshop/exception/CategoryExistsException.java
@@ -0,0 +1,7 @@
+package com.example.bookshop.exception;
+
+public class CategoryExistsException extends Exception {
+    public CategoryExistsException(String message) {
+        super(message);
+    }
+}
