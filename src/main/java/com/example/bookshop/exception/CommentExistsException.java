86f1433b5f553315cd7eb1b760223e55a0f85ece
larkes00 <zakirov2802@gmail.com>
2021-11-08 20:52:58 +0200

    Created exception for comment

---
+++ src/main/java/com/example/bookshop/exception/CommentExistsException.java
@@ -0,0 +1,7 @@
+package com.example.bookshop.exception;
+
+public class CommentExistsException extends Exception {
+    public CommentExistsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/bookshop/exception/CommentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.bookshop.exception;
+
+public class CommentNotFoundException extends Exception {
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+}
