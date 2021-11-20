c4badbfda3c42ad239a9f0b1a784fdd137352d00
larkes00 <zakirov2802@gmail.com>
2021-11-20 17:22:40 +0200

    CrossOrigin config

--- src/main/java/com/example/bookshop/security/WebConfig.java
+++ src/main/java/com/example/bookshop/security/WebConfig.java
@@ -1,18 +1,18 @@
 package com.example.bookshop.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000/", "https://book-shop-course.herokuapp.com/");
-        registry.addMapping("/login")
-                .allowedOrigins("http://localhost:3000/", "https://book-shop-course.herokuapp.com/");
+                .allowedOrigins("http://localhost:3000/", "https://book-shop-course.herokuapp.com/")
+                .allowedMethods("*");
     }
 }
