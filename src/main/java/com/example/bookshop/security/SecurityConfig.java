e222f82d52e859e9625c52dfc6c966a39568f89b
larkes00 <zakirov2802@gmail.com>
2021-11-26 14:30:02 +0200

    Modifying antMatchers for delete item from order

--- src/main/java/com/example/bookshop/security/SecurityConfig.java
+++ src/main/java/com/example/bookshop/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/v1/**")
                 .permitAll();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.DELETE, "/apu/v1/orders/{\\d+}/items/{\\d+}")
+                .antMatchers(HttpMethod.DELETE, "/apu/v1/orders/**")
                 .hasAnyAuthority("USER", "ADMIN");
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/api/v1/comments/", "/api/v1/orders/")
